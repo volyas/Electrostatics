c63e239915c2747b9b85bdf0debd8ae75b6ff43e
volyas <115228295+volyas@users.noreply.github.com>
2024-05-11 17:25:35 +0800

    Подправила построение неонородной сетки.

---
+++ DirectProblem/GridGenerator/Intervals/Splitting/NonUniformSplitter.cs
@@ -0,0 +1,73 @@
+﻿using DirectProblem.GridGenerator.Intervals.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectProblem.GridGenerator.Intervals.Splitting;
+
+public readonly record struct NonUniformSplitter : IIntervalSplitter
+{
+    public double DischargeRatio { get; }
+
+    private readonly double _step;
+    public NonUniformSplitter(double step, double dischargeRatio)
+    {
+        if (Math.Abs(dischargeRatio - 1d) < 1e-15)
+            throw new NotSupportedException();
+
+        _step = step;
+        DischargeRatio = dischargeRatio;
+    }
+
+    public IEnumerable<double> EnumerateValues(Interval interval)
+    {
+        var values = new List<double>();
+        var stepNumber = 0;
+        var value = 0d;
+
+        if (DischargeRatio < 1)
+        {
+            value = interval.End;
+            var dischargeRatio = 1 / DischargeRatio;
+
+            while (interval.Has(value))
+            {
+                values.Add(value);
+                var nextValue = value - _step * Math.Pow(dischargeRatio, stepNumber);
+
+                value = nextValue;
+                stepNumber++;
+
+                if (!(interval.Begin > value)) continue;
+                values.Add(interval.Begin);
+                break;
+            }
+
+            values.Reverse();
+        }
+        else
+        {
+            value = interval.Begin;
+            while (interval.Has(value))
+            {
+                values.Add(value);
+                var nextValue = value + _step * Math.Pow(DischargeRatio, stepNumber);
+
+                value = nextValue;
+                stepNumber++;
+
+                if (!(interval.End < value)) continue;
+                values.Add(interval.End);
+                break;
+            }
+        }
+
+        foreach (var pointValue in values)
+        {
+            yield return pointValue;
+        }
+    }
+}
+
