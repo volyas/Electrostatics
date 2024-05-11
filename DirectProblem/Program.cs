f000972cbc4de39053922d7ccb4346fc0c352acc
volyas <115228295+volyas@users.noreply.github.com>
2024-05-11 18:04:52 +0800

    Доработка.

--- DirectProblem/Program.cs
+++ DirectProblem/Program.cs
@@ -27,21 +27,16 @@ var gridBuilder2D = new GridBuilder2D();
 
 var grid = gridBuilder2D
  .SetRAxis(new AxisSplitParameter(new[]
-        { 0, 0.1, 1.1, 20.1, 100.1 },
+        { 0, 0.1, 5.1, 10.1 },
         new UniformSplitter(4),
-        new UniformSplitter(40),
-        new ProportionalSplitter(15, 1.45),
-        new ProportionalSplitter(5, 1.35)
+        new UniformSplitter(2),
+        new NonUniformSplitter(2, 1.05)
     ))
 .SetZAxis(new AxisSplitParameter(new[]
-        { -10d, -160d, -135d, -131d, -130d, -125d, -100d, 0d },
-        new ProportionalSplitter(5, 1 / 1.5),
-        new ProportionalSplitter(15, 1 / 1.48),
-        new UniformSplitter(156),
-        new UniformSplitter(39),
-        new UniformSplitter(195),
-        new ProportionalSplitter(15, 1.48),
-        new ProportionalSplitter(5, 1.5))
+        { -10d, -6d, -4d, 0d },
+        new NonUniformSplitter(2, 0.05),
+        new UniformSplitter(2),
+        new NonUniformSplitter(2, 1.05))
 )
    //вариант с новым разбиением
    .SetAreas(new Area[]
@@ -49,7 +44,7 @@ var grid = gridBuilder2D
        //скважина
        new(6, new Node2D(0d, -10d), new Node2D(0.1, 0d)),
        //первый слой
-       new(6, new Node2D(0.1, -10), new Node2D(10.1, 0d))
+       new(6, new Node2D(0.1, -10d), new Node2D(10.1, 0d))
    })
    //   .SetRAxis(new AxisSplitParameter(new[]
    //        { 0, 0.1, 1.1, 20.1, 100.1 },
@@ -126,14 +121,28 @@ var materialFactory = new MaterialFactory
 
 var current = 1d;
 
-var sources = new Source[59];
-var receiverLines = new ReceiverLine[59];
-var potentialDifferences = new double[59];
-var centersZ = new double[59];
+//var sources = new Source[59];
+//var receiverLines = new ReceiverLine[59];
+//var potentialDifferences = new double[59];
+//var centersZ = new double[59];
 
-for (var i = 0; i < 59; i++)
+//for (var i = 0; i < 59; i++)
+//{
+//    sources[i] = new Source(new Node2D(0.05, -100 - 1 * i), current);
+//    receiverLines[i] = new ReceiverLine(
+//        new Node2D(sources[i].Point.R, sources[i].Point.Z - 1), new Node2D(sources[i].Point.R, sources[i].Point.Z - 2)
+//    );
+//    centersZ[i] = (sources[i].Point.Z + receiverLines[i].PointN.Z) / 2;
+//}
+
+var sources = new Source[10];
+var receiverLines = new ReceiverLine[10];
+var potentialDifferences = new double[10];
+var centersZ = new double[10];
+
+for (var i = 0; i < 10; i++)
 {
-    sources[i] = new Source(new Node2D(0.05, -100 - 1 * i), current);
+    sources[i] = new Source(new Node2D(0.05, 0 - 1 * i), current);
     receiverLines[i] = new ReceiverLine(
         new Node2D(sources[i].Point.R, sources[i].Point.Z - 1), new Node2D(sources[i].Point.R, sources[i].Point.Z - 2)
     );
@@ -145,57 +154,57 @@ var localBasisFunctionsProvider = new LocalBasisFunctionsProvider(grid, new Line
 
 var firstBoundaryProvider = new FirstBoundaryProvider(grid);
 
-var firstConditions = firstBoundaryProvider.GetConditions(64, 430);
+var firstConditions = firstBoundaryProvider.GetConditions(10, 6);
+//var firstConditions = firstBoundaryProvider.GetConditions(64, 430);
 //var firstConditions = firstBoundaryProvider.GetConditions(12, 440);
 
 var directProblemSolver = new DirectProblemSolver();
 
 var resultO = new ResultIO("../DirectProblem/Results/");
 
-for (var i = 0; i < sources.Length; i++)
-{
-    var solution = directProblemSolver
-                .SetGrid(grid)
-                .SetMaterials(materialFactory)
-                .SetSource(sources[i])
-                ////.SetSource(sources[33])
-                //.SetSource(sources[30])
-                //.SetSource(new Source(new Node2D(0.05, -128), 1))
-                .SetFirstConditions(firstConditions)
-                .Solve();
+//for (var i = 0; i < sources.Length; i++)
+//{
+//    var solution = directProblemSolver
+//                .SetGrid(grid)
+//                .SetMaterials(materialFactory)
+//                .SetSource(sources[i])
+//                ////.SetSource(sources[33])
+//                //.SetSource(sources[5])
+//                //.SetSource(new Source(new Node2D(0.05, -128), 1))
+//                .SetFirstConditions(firstConditions)
+//                .Solve();
 
-    if (i == 31)
-    {
-        resultO.WriteResult(solution, "v2.dat");
-    }
+//    if (i == 31)
+//    {
+//        resultO.WriteResult(solution, "v2.dat");
+//    }
 
-    var femSolution = new FEMSolution(grid, solution, localBasisFunctionsProvider);
+//    var femSolution = new FEMSolution(grid, solution, localBasisFunctionsProvider);
 
-    var potentialM = femSolution.Calculate(receiverLines[i].PointM);
-    var potentialN = femSolution.Calculate(receiverLines[i].PointN);
+//    var potentialM = femSolution.Calculate(receiverLines[i].PointM);
+//    var potentialN = femSolution.Calculate(receiverLines[i].PointN);
 
-    //var potential = femSolution.Calculate(new Node2D(0.1, -130));
+//    //var potential = femSolution.Calculate(new Node2D(0.1, -130));
 
-    potentialDifferences[i] = potentialM - potentialN;
+//    potentialDifferences[i] = potentialM - potentialN;
 
-    CourseHolder.GetInfo(i, 0);
-}
+//    CourseHolder.GetInfo(i, 0);
+//}
 
 //для вычисления значения в точке
 
-//var solution = directProblemSolver
-//    .SetSource(new Source(new Node2D(0.05, -131), 1))
-//    .AssembleSLAE()
-//    .Solve();
-//resultO.WriteResult(solution, "v2.dat");
-//var femSolution = new FEMSolution(grid, solution, localBasisFunctionsProvider);
+var solution = directProblemSolver
+    .SetSource(new Source(new Node2D(0.05, -131), 1))
+    .Solve();
+resultO.WriteResult(solution, "v2.dat");
+var femSolution = new FEMSolution(grid, solution, localBasisFunctionsProvider);
 
-//var potential = new double[9];
-//for (var i = 0; i < 9; i++)
-//{
-//    potential[i] = femSolution.Calculate(new Node2D(0.1, -135 + i));
-//    Console.WriteLine(potential[i]);
-//}
+var potential = new double[9];
+for (var i = 0; i < 9; i++)
+{
+    potential[i] = femSolution.Calculate(new Node2D(0.1, -135 + i));
+    Console.WriteLine(potential[i]);
+}
 
 //
 
